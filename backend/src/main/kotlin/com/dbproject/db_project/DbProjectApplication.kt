3201fae90eeef757fa40205aaa447a5cfdc62438
spreadone <dong0524woo@naver.com>
2024-12-02 15:17:20 +0900

    setting

---
+++ backend/src/main/kotlin/com/dbproject/db_project/DbProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.dbproject.db_project
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DbProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<DbProjectApplication>(*args)
+}
